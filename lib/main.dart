0a701c0f161b819b419342f87b06e9f2c225c682
Max <slamax838@gmail.com>
2026-02-24 21:36:27 +0100

    recap

--- lib/main.dart
+++ lib/main.dart
@@ -4,56 +4,38 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
-          brightness: Brightness.dark,
-        ),
+        colorScheme: .fromSeed(seedColor: Colors.teal, brightness: .dark),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    //HERE2
-    return Scaffold(
-      appBar: AppBar(title: Text('Flutter'), centerTitle: true),
-
-      body: currentIndex == 0
-          ? Center(child: Text('State 1'))
-          : Center(child: Text('State 2')),
-
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        selectedIndex: currentIndex,
-        onDestinationSelected: (value) {
-          return setState(() {
-            currentIndex = value;
-          });
-        },
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter'), centerTitle: true),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          },
+        ),
       ),
     );
   }
