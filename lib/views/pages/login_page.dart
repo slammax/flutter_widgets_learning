c45c3e4cf96aa7dca27c1c845d85bbbcd7bdee19
Max <slamax838@gmail.com>
2026-03-05 21:09:08 +0100

    fixed lottie

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/views/widgets/hero_widget.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController controller = TextEditingController();
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +22,18 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(children: [HeroWidget(title: 'Login')]),
+        child: Column(
+          children: [
+            HeroWidget(title: 'Login'),
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,6 +1,7 @@
+import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/views/pages/login_page.dart';
-import 'package:flutter_widgets/views/widgets/hero_widget.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -13,11 +14,35 @@ class WelcomePage extends StatelessWidget {
         child: SafeArea(
           child: Column(
             mainAxisAlignment: .center,
-            mainAxisSize: .min,
+            mainAxisSize: .max,
             children: [
-              HeroWidget(title: 'Welcome'),
+              DotLottieLoader.fromAsset(
+                "assets/lotties/wave.lottie",
+                frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
+                  if (dotlottie != null) {
+                    return Lottie.memory(dotlottie.animations.values.single);
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
               SizedBox(height: 20),
               FilledButton(
+                style: FilledButton.styleFrom(minimumSize: Size(.infinity, 40)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginPage();
+                      },
+                    ),
+                  );
+                },
+
+                child: Text('Get Started'),
+              ),
+              TextButton(
                 onPressed: () {
                   Navigator.push(
                     context,
